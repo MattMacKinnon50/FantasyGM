7fa3b9a3b9920421d97525338db09a4863959d1a
Matt MacKinnon <matthew.mackinnon.50@gmail.com>
2018-05-06 12:07:57 -0400

    create table

--- db/migrate/20180503001204_create_team.rb
+++ db/migrate/20180503001204_create_team.rb
@@ -4,24 +4,24 @@ class CreateTeam < ActiveRecord::Migration[5.2]
       t.string :abbr, null: :false
       t.string :city, null: :false
       t.string :name, null: :false
-      t.string :conference, null: :false
-      t.string :division, null: :false
-      t.string :head_coach, null: :false
-      t.string :offensive_coordinator, null: :false
-      t.string :defensive_coordinator, null: :false
-      t.string :special_teams_coach, null: :false
-      t.string :primary_color, null: :false
-      t.string :secondary_color, null: :false
+      t.string :conference
+      t.string :division
+      t.string :head_coach
+      t.string :offensive_coordinator
+      t.string :defensive_coordinator
+      t.string :special_teams_coach
+      t.string :primary_color
+      t.string :secondary_color
       t.string :tertiary_color
       t.string :quaternary_color
-      t.string :wikipedia_logo_url, null: :false
-      t.string :wikipedia_wordmark_url, null: :false
-      t.string :stadium_name, null: :false
-      t.string :stadium_city, null: :false
-      t.string :stadium_state, null: :false
-      t.string :stadium_country, null: :false
-      t.string :stadium_capacity, null: :false
-      t.string :stadium_playing_surface, null: :false
+      t.string :wikipedia_logo_url
+      t.string :wikipedia_wordmark_url
+      t.string :stadium_name
+      t.string :stadium_city
+      t.string :stadium_state
+      t.string :stadium_country
+      t.string :stadium_capacity
+      t.string :stadium_playing_surface
       t.timestamps
     end
   end
---
+++ db/migrate/20180506153123_create_rosters.rb
@@ -0,0 +1,37 @@
+class CreateRosters < ActiveRecord::Migration[5.2]
+  def change
+    create_table :players do |t|
+      belongs_to :team
+      t.string :nfl_team, null: false
+      t.string :number,
+      t.string :first_name, null: :false
+      t.string :last_name, null: :false
+      t.string :position
+      t.string :height
+      t.string :weight
+      t.string :birth_date
+      t.string :college
+      t.integer :experience
+      t.string :photo_url
+      t.string :bye_week
+      t.string :college_draft_team
+      t.string :college_draft_year
+      t.string :college_draft_round
+      t.string :college_draft_pick
+      t.boolean :undrafted_free_agent_status
+      t.integer :fantasy_alarm_player_id
+      t.string :sports_radar_player_id
+      t.integer :rotoworld_player_id
+      t.integer :rotowire_player_id
+      t.integer :stats_player_id
+      t.integer :sports_direct_player_id
+      t.integer :xmlteam_player_id
+      t.integer :fanduel_player_id
+      t.integer :draftkings_player_id
+      t.integer :yahoo_player_id
+      t.integer :fantasydraft_player_id
+      t.integer :fantasy_stats_2017
+      t.integer :fantasy_stats_ppr_2017
+    end
+  end
+end
