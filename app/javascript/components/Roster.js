8e4777f45345c766aa9037ac9e8ad443c010b720
Matt MacKinnon <matthew.mackinnon.50@gmail.com>
2018-05-08 16:35:54 -0400

    WIP: built logic for starters

--- app/javascript/components/Roster.js
+++ app/javascript/components/Roster.js
@@ -5,12 +5,13 @@ class Roster extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      lineup: {qb: null, rb1: null, rb2: null, wr1: null, wr2: null, wr3: null, wr4: null, te1: null, teWrRb: null, ot1: null, ot2: null, g1: null, g2: null, c: null, dt1: null, dt2: null, de1: null, de2: null, ilb1: null, ilb2: null, olb1: null, olb2: null, cb1: null, cb2: null, cb3: null, ss: null, fs: null, p: null, k: null},
+      lineup: {qb: {name: "QB", positions: ["QB"], player: null}, rb1: {name: "RB", positions: ["RB"], player: null}, rb2: {name: "RB", positions: ["RB"], player: null}, wr1: {name: "WR", positions: ["WR"], player: null}, wr2: {name: "WR", positions: ["WR"], player: null}, wr3: {name: "WR", positions: ["WR"], player: null}, wr4: {name: "WR", positions: ["WR"], player: null}, te: {name: "TE", positions: ["TE"], player: null}, teWrRb: {name: "TE / WR / RB", positions: ["TE", "WR", "RB"], player: null}, ot1: {name: "OT", positions: ["OT"], player: null}, ot2: {name: "OT", positions: ["OT"], player: null}, g1: {name: "G", positions: ["G"], player: null}, g2: {name: "G", positions: ["G"], player: null}, c: {name: "C", positions: ["C"], player: null}, dt1: {name: "DT", positions: ["DT"], player: null}, dt2: {name: "DT", positions: ["DT"], player: null}, de1: {name: "DE", positions: ["DE"], player: null}, de2: {name: "DE", positions: ["DE"], player: null}, ilb1: {name: "ILB", positions: ["ILB", "LB"], player: null}, ilb2: {name: "ILB", positions: ["ILB", "LB"], player: null}, olb1: {name: "OLB", positions: ["OLB, LB"], player: null}, olb2: {name: "OLB", positions: ["OLB, LB"], player: null}, cb1: {name: "CB", positions: ["CB"], player: null}, cb2: {name: "CB", positions: ["CB"], player: null}, cb3: {name: "CB", positions: ["CB"], player: null}, ss: {name: "SS", positions: ["SS", "S"], player: null}, fs: {name: "FS", positions: ["FS", "S"], player: null}, p: {name: "P", positions: ["P"], player: null}, k: {name: "K", positions: ["K"], player: null}},
       players: [],
     }
     this.triggerFetch = this.triggerFetch.bind(this)
     this.setLineup = this.setLineup.bind(this)
-    this.generateSlots = this.generateSlots.bind(this)
+    this.generateRosterSlots = this.generateRosterSlots.bind(this)
+    this.generateBench = this.generateBench.bind(this)
   }
 
 
@@ -53,23 +54,39 @@ class Roster extends Component {
           starter = player
           }
         })
-      lineup[key] = starter
+      lineup[key]["player"] = starter
     }
     this.setState({ lineup: lineup })
   }
 
-  generateSlots() {
+  generateRosterSlots() {
     const lineup = this.state.lineup
+    const allPlayers = this.state.players
     const slots = []
     for (var key in lineup) {
       if (!lineup.hasOwnProperty(key)) continue;
-      var player = lineup[key]
+      let player = lineup[key]["player"]
+      let eligiblePositions = lineup[key]["positions"]
+      let  eligiblePlayers = []
+
+      allPlayers.forEach((player) => {
+          eligiblePositions.forEach((position) => {
+            if (player["position"] == position) {
+            eligiblePlayers.push(player)
+            }
+          })
+        })
+
       if (player != null) {
         slots.push(
             <Slot
               key={key}
+              className={"starting-player"}
               slot={key}
-              name={player["name"]}
+              playerName={player["name"]}
+              playerId={player["id"]}
+              positionName={lineup[key]["name"]}
+              eligiblePlayers={eligiblePlayers}
               position={player["position"]}
               number={player["number"]}
               nflTeam={player["nfl_team"]}
@@ -82,8 +99,12 @@ class Roster extends Component {
           slots.push(
               <Slot
                 key={key}
+                className={"empty-slot"}
                 slot={key}
-                name={"Empty"}
+                playerName={"Empty"}
+                positionName={lineup[key]["name"]}
+                eligiblePlayers={eligiblePlayers}
+                playerId={null}
                 position= {null}
                 number={null}
                 nflTeam={null}
@@ -94,16 +115,20 @@ class Roster extends Component {
             )
         }
     }
-    debugger
     return slots
   }
 
+  generateBench() {
+}
+
   render() {
-    this.generateSlots()
-    debugger
+    let slots = this.generateRosterSlots()
     return (
-      <div id="starters">
-        { slots }
+      <div>
+        <h3>Starting Roster:</h3>
+        <div id="starters">
+          { slots }
+        </div>
       </div>
       )
   }
--- app/javascript/components/Slot.js
+++ app/javascript/components/Slot.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Slot = props => {
   return (
     <div>
-      {}
+      
     </div>
   )
 }
