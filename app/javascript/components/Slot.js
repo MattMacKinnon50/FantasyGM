9890198dbf6041969aa84919b0859db53b92dcf3
Matt MacKinnon <matthew.mackinnon.50@gmail.com>
2018-05-08 20:19:25 -0400

    WIP: save checkpoint

--- app/javascript/components/Slot.js
+++ app/javascript/components/Slot.js
@@ -1,27 +1,63 @@
-import React from 'react'
+import React, { Component } from 'react';
 
-const Slot = props => {
-  if (props.starter) {
-    if (props.playerId != null) {
-    return (
-      <li className= {props.className}>
-        <p>{props.positionName} - {props.number} <a href={`/players/${props.playerId}`}>{props.playerName}</a> - {props.position} {props.nflTeam} - Bye Week: {props.byeWeek}</p>
-      </li>
-    )
-  } else{
+class Slot extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMenu: false,
+    }
+  }
+
+  render() {
+    if (this.props.starter) {
+      if (this.props.playerId != null) {
       return (
-      <li className= {props.className}>
-        <p>{props.positionName} - Empty Roster Spot </p>
-      </li>
+        <li className= {this.props.className}>
+          <p>{this.props.positionName} - {this.props.number} <a href={`/players/${this.props.playerId}`}>{this.props.playerName}</a> - {this.props.position} {this.props.nflTeam} - Bye Week: {this.props.byeWeek}</p>
+          <div>
+              <button>
+                Show menu
+              </button>
+                {if (this.state.showMenu) {
+              <div className="menu">
+                <button> Menu item 1 </button>
+                <button> Menu item 2 </button>
+                <button> Menu item 3 </button>
+              </div>
+            }
+          </div>
+
+        </li>
+      )
+    } else{
+        return (
+        <li className= {this.props.className}>
+          <p>{this.props.positionName} - Empty Roster Spot </p>
+          <div>
+            <button>
+              Show menu
+            </button>
+            {if (this.state.showMenu) {
+                <div className="menu">
+                  <button> Menu item 1 </button>
+                  <button> Menu item 2 </button>
+                  <button> Menu item 3 </button>
+                </div>
+              }
+            </div>
+          </div>
+        </li>
+        )
+      }
+    } else {
+      return (
+        <li className= {this.props.className}>
+          <p>{this.props.number} <a href={`/players/${this.props.playerId}`}>{this.props.playerName}</a> - {this.props.position} {this.props.nflTeam} - Bye Week: {this.props.byeWeek}</p>
+        </li>
       )
     }
-  } else {
-    return (
-      <li className= {props.className}>
-        <p>{props.number} <a href={`/players/${props.playerId}`}>{props.playerName}</a> - {props.position} {props.nflTeam} - Bye Week: {props.byeWeek}</p>
-      </li>
-    )
   }
 }
+
 
 export default Slot
