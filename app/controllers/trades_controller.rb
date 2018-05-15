8c696dc36feb16abff612562f5b99b6278a563f7
Matt MacKinnon <matthew.mackinnon.50@gmail.com>
2018-05-15 11:10:16 -0400

    functionality added

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -19,6 +19,45 @@ class TradesController < ApplicationController
     end
   end
 
+  def update
+    trade = Trade.find(params["id"])
+    team1 = trade.team1
+    team2 = trade.team2
+    trade.team1_players.each do |player|
+      if !team1.players.include?(player)
+        trade.destroy
+        flash[:notice] = "#{player.name} is no longer a member of #{team1.full_name} and is unavailable to trade.  This trade has been canceled."
+        redirect_back(fallback_location: "/trades")
+      end
+    end
+    trade.team2_players.each do |player|
+      if !team2.players.include?(player)
+        trade.destroy
+        flash[:notice] = "#{player.name} is no longer a member of #{team2.full_name} and is unavailable to trade.  This trade has been canceled."
+        redirect_back(fallback_location: "/trades")
+      end
+    end
+    trade.team1_players.each do |player|
+      player.team = team2
+      player.role = "b"
+      player.save
+    end
+    trade.team2_players.each do |player|
+      player.team = team1
+      player.role = "b"
+      player.save
+    end
+    trade.destroy
+    flash[:notice] = "Trade processed."
+    redirect_back(fallback_location: "/trades")
+  end
+
+  def destroy
+    trade = Trade.find(params[:id])
+    trade.destroy
+    redirect_back(fallback_location: "/trades")
+  end
+
   private
 
   def set_team
