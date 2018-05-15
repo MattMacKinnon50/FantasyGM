21d36fa813420ffb8e9f96112298321ff96118c6
Matt MacKinnon <matthew.mackinnon.50@gmail.com>
2018-05-15 09:30:32 -0400

    WIP trades index and show added

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -4,6 +4,12 @@ class TradesController < ApplicationController
     @trades = Trade.all.reverse
   end
 
+  def show
+    @team = Team.find(params["team_id"])
+    @review_trades = @team.trades.reverse
+    @pending_trades = Trade.where(team1_id: params["team_id"])
+  end
+
   def new
     @current_team = Team.find(current_user.team_id)
     @teams = Team.where.not("id = 33 OR id = ?", @current_team.id )
