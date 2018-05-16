6c51012504da0a5eaa56299c5a89fa159689bcf1
Matt MacKinnon <matthew.mackinnon.50@gmail.com>
2018-05-16 13:52:33 -0400

    wip heroku

--- app/builders/fa_build.rb
+++ app/builders/fa_build.rb
@@ -7,7 +7,7 @@ require 'json'
 
   request = Net::HTTP::Get.new(uri.request_uri)
   # Request headers
-  request['Ocp-Apim-Subscription-Key'] = FANTASY_DATA_API_TOKEN
+  request['Ocp-Apim-Subscription-Key'] = ENV['FANTASY_DATA_API_TOKEN']
   request.body = "{body}"
 
   response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
