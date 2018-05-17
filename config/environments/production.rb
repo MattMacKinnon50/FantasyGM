5c142af6580abcb14ebfcfd30e63ff0a7b7b24b7
Matt MacKinnon <matthew.mackinnon.50@gmail.com>
2018-05-17 19:05:52 -0400

    bug fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,6 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  config.active_record.dump_schema_after_migration = false 
+  config.action_mailer.default_url_options = { :host => 'fantasy-gm.herokuapp.com' }
 end
