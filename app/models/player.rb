680688ed92867286980d062e3125defc1de5b87f
Matt MacKinnon <matthew.mackinnon.50@gmail.com>
2018-05-17 18:39:35 -0400

    search complete

--- app/models/player.rb
+++ app/models/player.rb
@@ -18,4 +18,21 @@ class Player < ApplicationRecord
     position_names = {k: "Kicker", qb: "Quarterback", rb: "Running Back", wr: "Wide Receiver", te: "Tight End", ot: "Offensive Tackle", cb: "Corner Back", de: "Defensive End", fs: "Free Safety", g: "Guard", olb: "Outside Linebacker", ss: "Strong Safety", ilb: "Inside Linebacker", dt:  "Defensive Tackle", c: "Center", p: "Punter", fb: "Full Back", nt: "Nose Tackle", s: "Safety", lb: "Linebacker", ls: "Long Snapper", db: "Defensive Back", dl: "Defensive Line", ol: "Offensive Line"}
     position_names[position.downcase.to_sym]
   end
+
+  def self.search_by_full_name(search)
+    results = []
+    names = search.split(" ")
+    i=0
+    while i < names.length
+      new_results = Player.where("last_name ILIKE ?", "%#{names[i]}%").or(Player.where("first_name ILIKE ?", "%#{names[i]}%"))
+      new_results.each do |result|
+        if !results.include?(result)
+          results << result
+        end
+      end
+      i += 1
+    end
+    results
+  end
+
 end
