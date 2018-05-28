78fce38471850267a90944f231557a0616d0cc55
Matt MacKinnon <matthew.mackinnon.50@gmail.com>
2018-05-28 17:44:49 -0400

    WIP

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -8,5 +8,31 @@ validates :total, presence: true
 validates :aav, presence: true
 validates :guaranteed, presence: true
 
+  def guaranteed_int
+    result = guaranteed.gsub(/[$,]/, '').to_f
+    result.modulo(1) == 0 ? result.to_i : sprintf("%.2f", result)
+  end
+
+  def aav_int
+    result = aav.gsub(/[$,]/, '').to_f
+    result.modulo(1) == 0 ? result.to_i : sprintf("%.2f", result)
+  end
+
+  def total_int
+    result = total.gsub(/[$,]/, '').to_f
+    result.modulo(1) == 0 ? result.to_i : sprintf("%.2f", result)
+  end
+
+  def current_year
+    league = League.first
+    current_year = (league.league_year - start_year) + 1
+    current_year.to_s
+  end
+
+  def contract_by_year
+    contract_breakdown = {}
+    if length == 1
+      contract_breakdown("1") = {year: start_year, }
+
 
 end
